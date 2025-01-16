2610a376d776c1d73eef4500d81ec7a0057faa92
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-16 15:58:19 +0100

    Folder 01 completed

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,13 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto (nome){
+    return `ciao ${nome}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto())
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(saluto(userName));
