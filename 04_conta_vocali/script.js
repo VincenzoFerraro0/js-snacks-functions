6c94a163e00ae6e76690ccded91be2194ee52680
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-16 23:43:14 +0100

    Folder 04 completed

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,10 +1,29 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno
+    MINI TASK
+        - Dichiariamo una variabile con le vocali
+        - Dichiariamo una variabile il numero di strat pari a 0
+        - Creiamo una funzione con il parametro da controllare
+        - creiamo un ciclo che analizza la stringa
+        - creiamo una condizione che verifica se la variabile vocali è inclusa nel parametro 
+        - stampiamo e invochiamo la funzione con il parametro adeguato
+*/
 
 const word = 'javascript';
-
+let vocali = ['a', 'e', 'i', 'o', 'u'];
+let numeroVocali = 0;
 
 // Dichiara la funzione qui.
+function contaVocali(parola){
+    parola = parola.toLowerCase(); // il parametro inserito diventa tutto minuscolo 
+    for (let i = 0; i < parola.length; i++) {
+        if (vocali.includes(parola[i])) {
+          numeroVocali++;
+        }
+    }
+    return numeroVocali
+}
 
+console.log(contaVocali(word));
 
 // Invoca la funzione qui e stampa il risultato in console
 
